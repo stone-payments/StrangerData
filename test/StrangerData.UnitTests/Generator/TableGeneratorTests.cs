716421272ef5d824811b829641284d916a569c7f
Renan Rezende | Stone <rrezende@stone.com.br>
2021-03-21 19:07:44 -0300

    [@rengenesio] Improve TableGeneratur unit tests.

--- test/StrangerData.UnitTests/Generator/TableGeneratorTests.cs
+++ test/StrangerData.UnitTests/Generator/TableGeneratorTests.cs
@@ -7,7 +7,7 @@ namespace StrangerData.UnitTests.Generator
     public class TableGeneratorTests
     {
         [Fact]
-        public void Constructor_TablesFromDifferentDatabasesWithSameName_GetTableSchemaInfoFromBothDatabases()
+        public void Constructor_TablesFromDatabasesWithDifferentConnectionString_GetTableSchemaInfoFromBothDatabases()
         {
             // Arrange
             Mock<IDbDialect> databaseOneDialectMock = new Mock<IDbDialect>();
@@ -28,6 +28,31 @@ namespace StrangerData.UnitTests.Generator
             // Assert
             databaseOneDialectMock.Verify(d => d.GetTableSchemaInfo(tableName), Times.Once);
             databaseTwoDialectMock.Verify(d => d.GetTableSchemaInfo(tableName), Times.Once);
+        }
+
+        [Fact]
+        public void Constructor_TablesFromDifferentDialectsWithSameConnectionString_GetTableSchemaInfoFromCache()
+        {
+            // Arrange
+            string connectionString = Any.String();
+            string tableName = Any.String();
+
+            Mock<IDbDialect> databaseOneDialectMock = new Mock<IDbDialect>();
+            databaseOneDialectMock.Setup(d => d.ConnectionString)
+                .Returns(connectionString);
+
+            Mock<IDbDialect> databaseTwoDialectMock = new Mock<IDbDialect>();
+            databaseTwoDialectMock.Setup(d => d.ConnectionString)
+                .Returns(connectionString);
+
+            TableGenerator databaseOneTableGenerator = new TableGenerator(databaseOneDialectMock.Object, tableName);
+
+            // Act
+            TableGenerator databaseTwoTableGenerator = new TableGenerator(databaseTwoDialectMock.Object, tableName);
+
+            // Assert
+            databaseOneDialectMock.Verify(d => d.GetTableSchemaInfo(tableName), Times.Once);
+            databaseTwoDialectMock.Verify(d => d.GetTableSchemaInfo(tableName), Times.Never);
         }
     }
 }
